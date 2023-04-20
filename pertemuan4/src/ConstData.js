0fbdc7ecd7707eee444d1462a508d79c9c1054fd
anggaputra10 <114461008+anggaputra10@users.noreply.github.com>
2023-04-20 12:21:27 +0700

    Add files via upload

---
+++ pertemuan4/src/ConstData.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Angga", username: "anjotttttttt", gender: "M", is_view: true, cap: "emang ea"},
+    { id: 2, name: "agus", username: "agusstwn", gender: "M", is_view: true, cap: "yu bisa yu" },
+    { id: 3, name: "zaidan", username: "anndzn", gender: "M", is_view: true, cap: "silucu gasiii"},
+    { id: 4, name: "Subhan", username: "kevinn", gender: "M", is_view: true, cap: "Apa kabar saudara saudari" },
+    { id: 5, name: "Basor", username: "Bazzlightyear", gender: "M", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 6, name: "Kevina", username: "acililul", gender: "F", is_view: true, cap: "indahnya menjadi bulol" },
+    { id: 7, name: "agum", username: "agumkrn16", gender: "M", is_view: true, cap: "Mau kemana kawan kawan" },
+    { id: 8, name: "billy", username: "murelliyyann", gender: "F", is_view: true, cap: "cibinong calling" },
+    { id: 9, name: "ido", username: "radiokusutt", gender: "F", is_view: true, cap: "tempat ternyaman untuk pulang adalah bojong" },
+    { id: 10, name: "ojan", username: "fauzanrnd", gender: "M", is_view: true, cap: "Kemana ya kita?" }
+];
+
+export { UserObj };
